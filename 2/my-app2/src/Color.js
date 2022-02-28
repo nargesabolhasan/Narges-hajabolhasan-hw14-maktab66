c804a5a1d8edafb1fd98d5407f8c018efa41571f
narges Hajabolhasan <narges.abolhasan@gmail.com>
2022-02-28 20:39:39 +0330

    create list of colors

--- 2/my-app2/src/Color.js
+++ 2/my-app2/src/Color.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
-
+import './colorCss.css'
 class Color extends Component {
     render() {
         return (
             <div>
-                hii
+                <ul className="ul">
+                    {this.props.title.map(i =>
+                        <li className="li">
+                            <span className="span" style={{ background:i}}></span>
+                            {i}
+                        </li>
+                    )}
+                </ul>
             </div>
         );
     }
--- 2/my-app2/src/ColorGenerator.js
+++ 2/my-app2/src/ColorGenerator.js
@@ -1,27 +1,30 @@
 import React, { Component } from 'react'
 import Button from './button'
+import Color from './Color';
+
 export default class ColorGenerator extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            colors:[]
+            colors: []
         }
-        this.generatRndoms=this.generatRndoms.bind(this)
+        this.generatRndoms = this.generatRndoms.bind(this)
     }
-    generatRndoms(){
-        var randomColor = Math.floor(Math.random()*16777215).toString(16);
+    generatRndoms() {
+        var randomColor = Math.floor(Math.random() * 16777215).toString(16);
         this.setState({
-            colors:["#"+randomColor,...this.state.colors]
+            colors: ["#" + randomColor, ...this.state.colors]
         })
         console.log(this.state.colors)
     }
     render() {
         return (
             <div >ColorGenerator
-                <Button/>
+                <Button clickHandler={this.generatRndoms} title={'Add Color'} />
+                <Color title={this.state.colors} background={this.state.colors} />
             </div>
-           
-            
+
+
         )
     }
 }
